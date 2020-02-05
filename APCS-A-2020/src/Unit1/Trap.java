590a68bbe32f1eeb88783abc6999039dac7f94b4
gharibd1096 <gharibd1096@CA-SU-F106-39.sduhsd.lan>
2020-02-05 11:31:47 -0800

    Unit 2 assignments done

---
+++ APCS-A-2020/src/Unit1/Trap.java
@@ -0,0 +1,14 @@
+package Unit1;
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( double base1, double base2, double height )
+	{
+		return (0.5)*(base1+base2)*height;
+	}
+}
