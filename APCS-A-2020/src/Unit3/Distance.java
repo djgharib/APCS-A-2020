1bc64b919559b91a227159ba5270af23b1726d8e
gharibd1096 <gharibd1096@CA-SU-F106-39.sduhsd.lan>
2020-02-06 10:42:31 -0800

    finished distance

--- APCS-A-2020/src/Unit3/Distance.java
+++ APCS-A-2020/src/Unit3/Distance.java
@@ -10,6 +10,7 @@ package Unit3;
 import java.util.Scanner; 
 import static java.lang.System.*;
 import static java.lang.Math.*;
+import static java.lang.Math.*;
 
 public class Distance
 {
@@ -18,18 +19,30 @@ public class Distance
 
 	public Distance()
 	{
+		setCoordinates(0,0,0,0);
+		xOne=0;
+		yOne=0;
+		xTwo=0;
+		yTwo=0;
 	}
 
 	public Distance(int x1, int y1, int x2, int y2)
 	{
+		setCoordinates(x1, y1, x2, y2);
 	}
 
 	public void setCoordinates(int x1, int y1, int x2, int y2)
 	{
+		xOne=x1;
+		yOne=y1;
+		xTwo=x2;
+		yTwo=y2;
 	}
 
 	public void calcDistance()
 	{
+		distance = Math.sqrt(Math.pow((xTwo-xOne),2)+Math.pow((yTwo-yOne),2));
+		System.out.println("distance == " + String.format("%.3f\n", distance));
 	}
 	
 	public double getDistance()
