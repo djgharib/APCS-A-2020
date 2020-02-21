4f8a739c5ba2e227ef3398bca1650243cd517b9c
gharibd1096 <gharibd1096@CA-SU-F106-39.sduhsd.lan>
2020-02-21 10:38:55 -0800

    a

--- APCS-A-2020/src/Unit7/BiggestDouble.java
+++ APCS-A-2020/src/Unit7/BiggestDouble.java
@@ -1,34 +1,45 @@
 package Unit7;
 //(c) A+ Computer Science
+
 // www.apluscompsci.com
 //Name -  
 
 import static java.lang.System.*;
 
-public class BiggestDouble
-{
-	private double one,two,three,four;
+public class BiggestDouble {
+	private double one, two, three, four;
 
-	public BiggestDouble()
-	{
-		this(0,0,0,0);
+	public BiggestDouble() {
+		this(0, 0, 0, 0);
 	}
 
-	public BiggestDouble(double a, double b, double c, double d)
-	{
+	public BiggestDouble(double a, double b, double c, double d) {
+		one = a;
+		two = b;
+		three = c;
+		four = d;
 	}
 
-	public void setDoubles(double a, double b, double c, double d)
-	{
+	public void setDoubles(double a, double b, double c, double d) {
+		one = a;
+		two = b;
+		three = c;
+		four = d;
 	}
 
-	public double getBiggest()
-	{
-		return 0.0;
+	public double getBiggest() {
+		if (one > two && one > three && one > four) {
+			return one;
+		} else if (two > three && two > four) {
+			return two;
+		} else if (three > four) {
+			return three;
+		} else {
+			return four;
+		}
 	}
 
-	public String toString()
-	{
-	   return "";
+	public String toString() {
+		return "";
 	}
 }
