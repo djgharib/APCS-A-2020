c4d471b9f36b3fd0b50fe70ef8bdca2741c2040b
gharibd1096 <gharibd1096@CA-SU-F106-39.sduhsd.lan>
2020-02-24 11:12:15 -0800

    a

--- APCS-A-2020/src/Unit7/TriangleFive.java
+++ APCS-A-2020/src/Unit7/TriangleFive.java
@@ -1,34 +1,49 @@
 package Unit7;
 //(c) A+ Computer Science
+
 //www.apluscompsci.com
 //Name -
 
 import static java.lang.System.*;
 
-public class TriangleFive
-{
-   private char letter;
-   private int amount;
+public class TriangleFive {
+	private static char letter;
+	private static int amount;
+	private static char letterOverride;
 
-	public TriangleFive()
-	{
+	public TriangleFive() {
 	}
 
-	public TriangleFive(char c, int amt)
-	{
+	public TriangleFive(char c, int amt) {
+		letter = c;
+		amount = amt;
 	}
 
-	public void setLetter(char c)
-	{
+	public static void setLetter(char c) {
+		letter = c;
+		letterOverride = c;
 	}
 
-	public void setAmount(int amt)
-	{
+	public static void setAmount(int amt) {
+		amount = amt;
+		for (int z = amount; z > 0; z--) {
+			for (int n = amount; n > 0; n--) {
+				for (int i = amount; i > 0; i--) {
+					System.out.print(letter);
+				}
+				System.out.print(" ");
+				letter--;
+				amount--;
+			}
+			amount = amt;
+			letter = letterOverride;
+			System.out.println();
+		}
 	}
 
-	public String toString()
-	{
-		String output="";
+	public String toString() {
+
+		String output = "";
 		return output;
 	}
 }
--- APCS-A-2020/src/Unit7/TriangleFiveRunner.java
+++ APCS-A-2020/src/Unit7/TriangleFiveRunner.java
@@ -1,13 +1,15 @@
 package Unit7;
 //(c) A+ Computer Science
+
 //www.apluscompsci.com
 //Name -
 
 import static java.lang.System.*;
 
-public class TriangleFiveRunner
-{
-   public static void main(String args[])
-   {
+public class TriangleFiveRunner {
+	public static void main(String args[]) {
+		TriangleFive tri = new TriangleFive();
+		TriangleFive.setLetter('C');
+		TriangleFive.setAmount(4);
 	}
 }
