a7b4aadfc78e750a6c9c39b992f9539465677566
BuildTools <unconfigured@null.spigotmc.org>
2020-02-26 23:44:35 -0800

    oddToEven done

--- APCS-A-2020/src/Unit8/OddToEvenRunner.java
+++ APCS-A-2020/src/Unit8/OddToEvenRunner.java
@@ -8,5 +8,18 @@ public class OddToEvenRunner
 {
 	public static void main( String args[] )
 	{
+		int[] array = {7,1,5,3,11,5,6,7,8,9,10,12345,11};
+		System.out.println(RayOddToEven.go(array));
+		
+		int[] array2 = {11,9,8,7,6,5,4,3,2,1,-99,7};
+		System.out.println(RayOddToEven.go(array2));
+		
+		int[] array3 = {9,11,11,11,7,1000,3};
+		System.out.println(RayOddToEven.go(array3));
+		
+		int[] array4 = {2,4,6,8,8};
+		System.out.println(RayOddToEven.go(array4));
+		
+		
 	}
 }
--- APCS-A-2020/src/Unit8/RayOddToEven.java
+++ APCS-A-2020/src/Unit8/RayOddToEven.java
@@ -6,8 +6,36 @@ package Unit8;
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public static int go(int[] ray)
 	{
-		return false;
+		int oddPos=0;
+		int evenPos = 0;
+		for(int i = 0; i<ray.length; i++) {
+			if(findOdd(ray[i])==true) {
+				oddPos = i;
+				for(int x = i; x<ray.length; x++) {
+					if(findOdd(ray[x])==false) {
+						evenPos = x;
+						return Math.abs(oddPos-evenPos);
+					}
+					if (findOdd(ray[x])==true && x==ray.length-1) {
+						return -1;
+					}
+				}
+			}
+			if (findOdd(ray[i])==false && i==ray.length-1) {
+				return -1;
+			}
+		}
+		return -1;
+	}
+	
+	public static boolean findOdd(int a) {
+		if(a%2==0) {
+			return false;
+		}
+		else {
+			return true;
+		}
 	}
 }
