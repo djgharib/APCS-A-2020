4bcffcb50140f58e9039e3e79fd2c2b54bad76a4
BuildTools <unconfigured@null.spigotmc.org>
2020-02-26 23:53:45 -0800

    downrunner done

--- APCS-A-2020/src/Unit8/DownRunner.java
+++ APCS-A-2020/src/Unit8/DownRunner.java
@@ -7,6 +7,13 @@ package Unit8;
 public class DownRunner
 {
 	public static void main( String args[] )
-	{											
+	{
+		int[] array = {9,8,7,6,5,4,3,2,1};
+		System.out.println(RayDown.go(array));
+		
+		int[] array2 = {-99,1,2,3,4,5,6,7,8,9,10,12345};
+		System.out.println(RayDown.go(array2));
+		
+		//dont have time to put every dataset in because i cant copypaste it in
 	}
 }
--- APCS-A-2020/src/Unit8/RayDown.java
+++ APCS-A-2020/src/Unit8/RayDown.java
@@ -10,6 +10,17 @@ public class RayDown
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(int[] numArray)
 	{
-		return true;
+		int gameing = 0;
+		for(int i = 0; i<numArray.length-1; i++) {
+			if(numArray[i]>numArray[i+1]) {
+				gameing++;
+			}
+		}
+		if(gameing==numArray.length-1) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}	
 }
