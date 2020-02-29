c4d155632e61ad282a116995532c82e1afbd0c04
BuildTools <unconfigured@null.spigotmc.org>
2020-02-28 23:53:34 -0800

    oddeven

--- APCS-A-2020/src/Unit9/ListOddToEven.java
+++ APCS-A-2020/src/Unit9/ListOddToEven.java
@@ -9,8 +9,17 @@ import java.util.List;
 
 public class ListOddToEven
 {
-	public static boolean go( List<Integer> ray )
+	public static int go( List<Integer> ray )
 	{
-		return false;
+		for(int i = 0; i<ray.size(); i++) {
+			if(ray.get(i)%2==1) {
+				for(int x=ray.size()-1; x>0; x--) {
+					if(ray.get(x)%2==0) {
+						return(x-i);
+					}
+				}
+			}
+		}
+		return -1;
 	}
 }
--- APCS-A-2020/src/Unit9/OddToEvenRunner.java
+++ APCS-A-2020/src/Unit9/OddToEvenRunner.java
@@ -1,4 +1,7 @@
 package Unit9;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
@@ -8,5 +11,20 @@ public class OddToEvenRunner
 {
 	public static void main( String args[] )
 	{
+		int[] game = {11,9,8,7,6,5,4,3,2,1,-99,7};
+		ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(7,1,5,3,11,5,6,7,8,9,10,12345,11));
+		System.out.println(ListOddToEven.go(arrayList));
+		
+		ArrayList<Integer> arrayList2 = new ArrayList<>(Arrays.asList(11,9,8,7,6,5,4,3,2,1,-99,7));
+		System.out.println(ListOddToEven.go(arrayList2));
+		
+		ArrayList<Integer> arrayList3 = new ArrayList<>(Arrays.asList(10,20,30,40,5,41,31,20,11,7));
+		System.out.println(ListOddToEven.go(arrayList3));
+		
+		ArrayList<Integer> arrayList4 = new ArrayList<>(Arrays.asList(32767,70,4,5,6,7));
+		System.out.println(ListOddToEven.go(arrayList4));
+		
+		ArrayList<Integer> arrayList5 = new ArrayList<>(Arrays.asList(2,7,11,21,5,7));
+		System.out.println(ListOddToEven.go(arrayList5));
 	}
 }
