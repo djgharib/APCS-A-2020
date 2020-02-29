4696ad285d76611a6c22a1e39c9d6466d96d9337
BuildTools <unconfigured@null.spigotmc.org>
2020-02-28 23:56:10 -0800

    fix

--- APCS-A-2020/src/Unit9/OddToEvenRunner.java
+++ APCS-A-2020/src/Unit9/OddToEvenRunner.java
@@ -11,7 +11,6 @@ public class OddToEvenRunner
 {
 	public static void main( String args[] )
 	{
-		int[] game = {11,9,8,7,6,5,4,3,2,1,-99,7};
 		ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(7,1,5,3,11,5,6,7,8,9,10,12345,11));
 		System.out.println(ListOddToEven.go(arrayList));
 		
