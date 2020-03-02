1d2ccf51aeaa1bd86a4c1b31ea3a6054fb2c51a8
gharibd1096 <gharibd1096@CA-SU-F106-39.sduhsd.lan>
2020-03-02 10:50:55 -0800

    triangleWord done

--- APCS-A-2020/src/Unit11/TriangleWord.java
+++ APCS-A-2020/src/Unit11/TriangleWord.java
@@ -12,5 +12,12 @@ public class TriangleWord
 		
 	public static void printTriangle(String word)
 	{
+		String tempWord = "";
+		for(int i = 0; i<=word.length(); i++) {
+			for(int x = 0; x<i; x++) {
+				System.out.print(word.substring(0,i));
+			}
+			System.out.println();
+		}
 	}
 }
--- APCS-A-2020/src/Unit11/TriangleWordRunner.java
+++ APCS-A-2020/src/Unit11/TriangleWordRunner.java
@@ -10,6 +10,16 @@ public class TriangleWordRunner
 {
 	public static void main( String args[] )
 	{
-		
+		Scanner keyboard = new Scanner(System.in);
+	   	String choice="";
+			do{
+				out.print("\nEnter a word :: ");
+				String word = keyboard.next();		
+				
+				TriangleWord.printTriangle(word);
+				
+				System.out.print("\nDo you want to enter more sample input? ");
+				choice=keyboard.next();			
+			}while(choice.equals("Y")||choice.equals("y"));	
 	}
 }
