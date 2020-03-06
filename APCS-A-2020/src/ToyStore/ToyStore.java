e2e422f820372e750362f0d7f2365c4c9bdadc4b
BuildTools <unconfigured@null.spigotmc.org>
2020-03-06 15:29:28 -0800

    update

--- APCS-A-2020/src/ToyStore/ToyStore.java
+++ APCS-A-2020/src/ToyStore/ToyStore.java
@@ -6,7 +6,9 @@ package ToyStore;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+
 import static java.lang.System.*;
 
 public class ToyStore
@@ -19,6 +21,26 @@ public class ToyStore
 
 	public void loadToys( String toys )
 	{
+		int wordCount = 1;
+		for (int i = 0; i<toys.length(); i++) {
+			if(toys.charAt(i) == ' ') {
+				wordCount++;
+			}
+		}
+		String[] toyArray = new String[wordCount];
+		int lastSpace = 0;
+		wordCount = 0;
+		for (int i = 0; i<toys.length(); i++) {
+			if(toys.charAt(i) == ' ') {
+				toyArray[wordCount] = (toys.substring(lastSpace, i));
+				wordCount++;
+				lastSpace = i+1;
+			}
+			if(i == toys.length()-1) {
+				toyArray[wordCount] = (toys.substring(lastSpace, i+1));
+			}
+		}
+		System.out.println(Arrays.toString(toyArray));
 	}
   
   	public Toy getThatToy( String nm )
--- APCS-A-2020/src/ToyStore/ToyStoreRunner.java
+++ APCS-A-2020/src/ToyStore/ToyStoreRunner.java
@@ -12,6 +12,10 @@ public class ToyStoreRunner
 {
 	public static void main( String args[] )
 	{
-
+		ToyStore sto = new ToyStore();
+		System.out.println( sto );
+		sto.loadToys("sorry bat sorry sorry sorry train train teddy teddy ball ball");
+		System.out.println( sto );	
+		System.out.println( "max == " + sto.getMostFrequentToy() );		
 	}
 }
