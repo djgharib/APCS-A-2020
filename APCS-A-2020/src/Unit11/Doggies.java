d2ef551d0ab54bbe3bc3625db9033572a6048f90
BuildTools <unconfigured@null.spigotmc.org>
2020-03-06 22:39:24 -0800

    doggies done

--- APCS-A-2020/src/Unit11/Doggies.java
+++ APCS-A-2020/src/Unit11/Doggies.java
@@ -1,38 +1,46 @@
 package Unit11;
 //(c) A+ Computer Science
+
 //www.apluscompsci.com
 //Name -
 
 import java.util.Arrays;
 
-public class Doggies
-{
+public class Doggies {
 	private Dog[] pups;
 
-	public Doggies(int size)
-	{
-		//point pups at a new arry of Dog
-		
-	}
-	
-	public void set(int spot, int age, String name)
-	{
-		//put a new Dog in the array at spot 
-		//make sure spot is in bounds		
+	public Doggies(int size) {
+		// point pups at a new array of Dog
+		pups = new Dog[size];
 	}
 
-	public String getNameOfOldest()
-	{
-		return null;
+	public void set(int spot, int age, String name) {
+		pups[spot] = new Dog(age, name);
+		// put a new Dog in the array at spot
+		// make sure spot is in bounds
 	}
 
-	public String getNameOfYoungest()
-	{
-		return null;
+	public String getNameOfOldest() {
+		int oldest = 0;
+		for(int i = 0; i<pups.length; i++) {
+			if(pups[oldest].getAge()<pups[i].getAge()) {
+				oldest = i;
+			}
+		}
+		return pups[oldest].getName();
 	}
 
-	public String toString()
-	{
-		return ""+Arrays.toString(pups);
+	public String getNameOfYoungest() {
+		int youngest = 0;
+		for(int i = 0; i<pups.length; i++) {
+			if(pups[youngest].getAge()>pups[i].getAge()) {
+				youngest = i;
+			}
+		}
+		return pups[youngest].getName();
+	}
+
+	public String toString() {
+		return "" + Arrays.toString(pups);
 	}
 }
