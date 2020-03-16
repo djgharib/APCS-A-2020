588a3b7292d9220df4542a5e41967bae60b7ebbc
BuildTools <unconfigured@null.spigotmc.org>
2020-03-16 14:21:03 -0700

    dataProject

---
+++ APCS-A-2020/src/DataProject/Cereal.java
@@ -0,0 +1,39 @@
+package DataProject;
+//which cereal is best for low carbs and high protein
+import java.util.ArrayList;
+public class Cereal {
+	private String name;
+	private int carbs, protein;
+	public Cereal (String n, int c, int p) {
+		name = n;
+		carbs = c;
+		protein = p;
+	}
+	public Cereal () {
+		
+	}
+	//set methods
+	public void setCarbs(int c) {
+		carbs = c;
+	}
+	public void setName(String n) {
+		name = n;
+	}
+	public void setProtein(int p) {
+		protein = p;
+	}
+	//get methods
+	public int getCarbs() {
+		return carbs;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getProtein() {
+		return protein;
+	}
+	//to string
+	public String toString() {
+		return this.name +" has " + this.carbs + " grams of carbs and " + protein +" grams of protein.";
+	}
+}
