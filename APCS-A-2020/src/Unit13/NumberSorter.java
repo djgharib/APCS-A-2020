ea0f5aecd5f3231fd0754c24594beae3001227c7
BuildTools <unconfigured@null.spigotmc.org>
2020-03-27 22:43:22 -0700

    numberSorter done

--- APCS-A-2020/src/Unit13/NumberSorter.java
+++ APCS-A-2020/src/Unit13/NumberSorter.java
@@ -3,6 +3,7 @@ package Unit13;
 //www.apluscompsci.com
 //Name -
 
+import java.util.ArrayList;
 import java.util.Arrays; 
 import java.util.Scanner;
 import java.io.File;
@@ -15,13 +16,31 @@ public class NumberSorter
 
 	private static int getNumDigits(int number)
 	{
-		int count = 0;
-		return count;
+		int length = 0;
+		int x = 1;
+		boolean lessThan = false;
+		while (lessThan == false) {
+			if(number / x == 0) {
+				lessThan = true;
+			}
+			else{
+				x = 10*x;
+				length++;
+			}
+		}
+		return length;
 	}
 
 	public static int[] getSortedDigitArray(int number)
 	{
-		int[] sorted = null;
+		int[] sorted = new int[getNumDigits(number)];
+		int y = 0;
+		while (number > 0) {
+		    sorted[y] = number % 10;
+		    number = number / 10;
+		    y++;
+		}
+		Arrays.sort(sorted);
 		return sorted;
 	}
 }
