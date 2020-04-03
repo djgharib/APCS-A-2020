03979eb46a17b07884ea4d0c32918619a1b78533
Daniel <Daniel@DESKTOP-ACHVDNM.attlocal.net>
2020-04-03 12:20:19 -0700

    a

--- APCS-A-2020/src/Unit13/NumberShifterRunner.java
+++ APCS-A-2020/src/Unit13/NumberShifterRunner.java
@@ -13,6 +13,17 @@ public class NumberShifterRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
+		int[] stuff = new int[] {3, 61, 15, 11, 7, 9, 2};
+		for(int i=1; i<5; ++i) {
+			int val = stuff[i];
+			int j=i;
+			while(j>0&&val<stuff[j-1]) {
+				stuff[j]=stuff[j-1];
+				j--;
+			}
+			stuff[j]=val;
+		}
+		System.out.println(Arrays.toString(stuff));
 		System.out.println(Arrays.toString(NumberShifter.shiftEm(NumberShifter.makeLucky7Array(20))));
 		System.out.println(Arrays.toString(NumberShifter.shiftEm(NumberShifter.makeLucky7Array(20))));
 		System.out.println();
