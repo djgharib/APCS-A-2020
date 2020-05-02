693320588e0ba84032cc433c204ca7f8265423e6
Daniel <Daniel@DESKTOP-ACHVDNM.attlocal.net>
2020-05-01 22:13:49 -0700

    a

--- APCS-A-2020/src/Pong/Paddle.java
+++ APCS-A-2020/src/Pong/Paddle.java
@@ -24,38 +24,56 @@ public class Paddle extends Block {
 
 	// add the other Paddle constructors
 	public Paddle(int x, int y) {
-		super(x,y);
+		super(x, y);
 		speed = 5;
 	}
-	
-	public Paddle(int x, int y, int spd) {
-		super(x,y);
-		speed = spd;
-	}
-	
-	public Paddle(int x, int y, int wid, int hei, int spd) {
-		super(x,y, wid, hei);
-		speed = spd;
-	}
-	
-	public Paddle(int x, int y, int wid, int hei, Color col, int spd) {
-		super(x,y, wid, hei, col);
-		speed = spd;
-	}
-	
-	public void moveUpAndDraw(Graphics window) {
 
+	public Paddle(int x, int y, int spd) {
+		super(x, y);
+		speed = spd;
+	}
+
+	public Paddle(int x, int y, int wid, int hei) {
+		super(x, y, wid, hei);
+		speed = 5;
+	}
+
+	public Paddle(int x, int y, int wid, int hei, int spd) {
+		super(x, y, wid, hei);
+		speed = spd;
+	}
+
+	public Paddle(int x, int y, int wid, int hei, Color col, int spd) {
+		super(x, y, wid, hei, col);
+		speed = spd;
+	}
+
+	public Paddle(int x, int y, int wid, int hei, Color col) {
+		super(x, y, wid, hei, col);
+		speed = 5;
+	}
+
+	public void moveUpAndDraw(Graphics window) {
+		draw(window, Color.white);
+
+		setY(getY() - speed);
+
+		draw(window, Color.black);
 	}
 
 	public void moveDownAndDraw(Graphics window) {
+		draw(window, Color.white);
 
+		setY(getY() + speed);
+
+		draw(window, Color.black);
 	}
 
 	// add get methods
-	public int getSpeed()
-	  {
-		 return speed;
-	  }
+	public int getSpeed() {
+		return speed;
+	}
+
 	// add a toString() method
 	public String toString() {
 		return super.toString() + " " + speed;
